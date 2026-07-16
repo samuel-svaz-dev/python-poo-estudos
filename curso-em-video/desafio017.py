1fee73efde295f4f4456e2b2a9dec52e5f73ddd7
Samuel Vaz <svaz.samuel@gmail.com>
2026-07-16 11:59:35 -0300

    feat: resolve desafio 017 classe produto

---
+++ curso-em-video/desafio017.py
@@ -0,0 +1,25 @@
+#Desafio 017: Crie a classe Produto, onde podemos cadastrar nome e o preço.
+#Crie também um método que mostre uma etiqueta de preço do produto.
+
+from rich import print
+from rich.panel import Panel
+
+class Produto:
+
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    
+    def etiqueta(self):
+        linha = '-' * 35
+        preco_formatado = f'R${self.preco}'
+        mostra = Panel(f'{self.nome} {linha} {preco_formatado}', title = 'Produto', width = 40)
+        print(mostra)
+    
+
+p1 = Produto('Iphone 17 Pro Max', 25_000.85)
+p1.etiqueta()
+
+p2 = Produto('NoteBook Gamer', 8_000.00)
+p2.etiqueta()
